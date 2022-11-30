da7217d2b0b98787244a44c6b71c3623f7835444
Jiheng Nie <109745413+JihengNie@users.noreply.github.com>
2022-11-30 11:34:24 -0800

    Time: 96 ms (64.60%), Space: 42.4 MB (71.89%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  if (!strs.length) return '';
+
+  let prefix = strs[0];
+
+  for (let i = strs.length; --i;) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (!prefix.length) return '';
+    }
+  }
+
+  return prefix;
+};
+
+
